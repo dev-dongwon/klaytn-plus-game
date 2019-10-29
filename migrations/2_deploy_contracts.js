e615620bf3b476697baa3fb5541cc0d82503f401
dev-dongwon <dev.dongwonkim@gmail.com>
2019-10-29 16:05:00 +0900

    FEAT : 배포를 위한 writefile 기능 추가
    ABI, address를 writefile해 파일로 생성

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,23 @@
 const fs = require('fs');
 const AdditionGame = artifacts.require('./AdditionGame.sol')
 
-module.exports = async function (deployer) {
-  deployer.deploy(AdditionGame);
+module.exports = function (deployer) {
+  deployer.deploy(AdditionGame)
+    .then(() => {
+      if (AdditionGame._json) {
+        fs.writeFile('deployedABI', JSON.stringify(AdditionGame._json.ABI),
+          (err) => {
+            if (err) throw err;
+            console.log('ABI 입력 성공');
+          }
+        )
+
+        fs.writeFile('deployedAddress', AdditionGame.address,
+          (err) => {
+            if (err) throw err;
+            console.log('파일에 주소 입력 성공');
+          }
+        )
+      }
+    })
 }
