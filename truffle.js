ef09904bb8f08b827a405d04d2e40fbfe108bc63
dev-dongwon <dev.dongwonkim@gmail.com>
2019-10-29 16:15:28 +0900

    FEAT : 클레이튼 테스트넷 배포를 위한 트러플 설정
    provider, network_id, gas, gasPrice 설정

--- truffle.js
+++ truffle.js
@@ -1,1 +1,17 @@
 // truffle.js config for klaytn.
+const PrivateKeyConnector = require('connect-privkey-to-provider');
+const NETWORK_ID = '1001' // 바오밥 고유 네트워크 아이디
+const GASLIMIT = '20000000';
+const URL = 'https://api.baobab.klaytn.net:8651'
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+module.exports = {
+  networks: {
+    klaytn: {
+      provider: new PrivateKeyConnector(PRIVATE_KEY, URL),
+      network_id : NETWORK_ID,
+      gas: GASLIMIT,
+      gasPrice: null,
+    }
+  }
+}
